5a387662b6cb45b23bff5ad46be850080b81f96e
cabdishakurr <cabdishakurrrr@gmail.com>
2024-08-24 17:42:14 +0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inventory Management Dashboard",
+
+    'summary': """
+        Power BI to Odoo Connection. Can we used as connector. Power Bi Connector , powerbi connector, Uses Odoo external api to connect with microsoft power bi. dashboard can be piblished also. Odoo data analysis, Odoo data visualization, Odoo database visualization, Odoo relationship diagram, Odoo SQL explorer, Odoo data explorer, Odoo schema mapping, Odoo AI analytics, Odoo AI visualization, Odoo data model AI, Odoo artificial intelligence, AI reports Odoo, AI dashboards Odoo, Odoo model diagram, Odoo schema explorer, Odoo database model, Odoo ERD visualization, Odoo DB structure, Odoo database schema, Odoo table visualization, Odoo data model mapping, Odoo schema diagram, Odoo SQL analysis, Odoo data model design, Odoo schema design, Odoo database design, Odoo data architecture, Odoo ERD model, Odoo DB visualization, Odoo database chart, Odoo schema structure, Odoo data model graph, Odoo table diagram, Odoo schema model, Odoo data model viewer, Odoo database diagramming, Odoo table structure, Odoo data relationship, Odoo model visualization, Odoo data structure, Odoo database mapping, visualize Odoo data, Odoo reports, Odoo analytics, Odoo dashboards. react, react js, react application, mern, mean, stack, node, node,js node js, react.js, fromtend, front end, front-end, ninja, ai, dashboard, analytics, reports, dashboards, report, sales dashboard, odoo sales, powerbi sales, power bi sales, reporting, sales dashboard, sales report, odoo sale dashboard, odoo sales dashboard, odoo sales report, sale, sale dashboard power bi dashboard, powerbi dashboard, dashboards.
+""",
+
+    'description': """
+        Power Bi reports and dashboard for Odoo data.
+    """,
+
+    'author': "TechFinna",
+    'website': "https://techfinna.com/",
+    'category': 'Analytics',
+    'price': 0,
+    'currency': 'USD',
+    'version': '1.0.0',
+    'installable': True,
+    'live_test_url': 'https://app.powerbi.com/view?r=eyJrIjoiNTVhZGQzNmQtNzk1My00NTVlLTg0N2ItOGVlMzcwNDFiMjI2IiwidCI6IjQ0N2UwMzI5LWVhOTItNGIyOC1iY2FiLWY3ZDlmNjcwMGJmZSJ9',
+    'support': "info@techfinna.com",
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    #'depends': ['base'],
+    'images': ['static/description/icon.png'],
+}
